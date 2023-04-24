53d285496dcf7f4663e2cd718a4e91d4b8325614
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-04-23 22:42:23 -0300

    feat: add spotlight hero

---
+++ sections/SpotlightHero.tsx
@@ -0,0 +1,11 @@
+export { default } from "deco-sites/bergerson/components/heros/SpotlightHero.tsx";
+
+export type {
+  HTML,
+  Image as LiveImage,
+} from "deco-sites/std/components/types.ts";
+
+export type {
+  Props,
+  Slide,
+} from "deco-sites/bergerson/components/heros/SpotlightHero.tsx";
