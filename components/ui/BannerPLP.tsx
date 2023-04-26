117b53bf14621d33fabdd4acf39967225a2aba89
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-04-26 01:00:49 -0300

    feat: add bannerplp extension

--- components/ui/BannerPLP.tsx
+++ components/ui/BannerPLP.tsx
@@ -1,5 +1,6 @@
 import type { LoaderReturnType } from "$live/types.ts";
 import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
+import Container from "deco-sites/bergerson/components/ui/Container.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
 import { RequestViewer } from "deco-sites/bergerson/functions/requestViewer.ts";
 
@@ -14,6 +15,10 @@ export interface Banner {
     /** @description image alt text */
     alt?: string;
   };
+  description?: {
+    logo: LiveImage;
+    text: string;
+  };
 }
 
 export interface Props {
@@ -22,24 +27,38 @@ export interface Props {
 }
 
 function BannerUI({ banner }: { banner: Banner }) {
-  const { image } = banner;
+  const { image, description } = banner;
 
   return (
-    <div class="grid grid-cols-1 grid-rows-1">
-      <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
-        <Source
-          width={360}
-          src={image.mobile}
-          media="(max-width: 767px)"
-        />
-        <Source
-          width={1440}
-          src={image.desktop}
-          media="(min-width: 767px)"
-        />
-        <img class="w-full" src={image.desktop} alt={image.alt} />
-      </Picture>
-    </div>
+    <>
+      <div class="grid grid-cols-1 grid-rows-1">
+        <Picture preload class="col-start-1 col-span-1 row-start-1 row-span-1">
+          <Source
+            width={360}
+            src={image.mobile}
+            media="(max-width: 767px)"
+          />
+          <Source
+            width={1440}
+            src={image.desktop}
+            media="(min-width: 767px)"
+          />
+          <img class="w-full" src={image.desktop} alt={image.alt} />
+        </Picture>
+      </div>
+
+      {description && (
+        <Container class="px-4 sm:py-10 flex flex-col gap-2 items-center justify-center">
+          <img
+            alt={image.alt}
+            src={description.logo}
+            class="mx-[15px] h-[115px] w-full lg:w-[200px] object-contain"
+          />
+
+          <p class="text-center">{description.text}</p>
+        </Container>
+      )}
+    </>
   );
 }
 
@@ -49,8 +68,7 @@ function Banner({ requestViewer, banners = [] }: Props) {
   }
 
   const url = new URL(requestViewer.request.url);
-  const pathname = url.pathname.toLocaleLowerCase();
-  const matching = banners.find(({ matcher }) => matcher === pathname);
+  const matching = banners.find(({ matcher }) => matcher === url.pathname);
 
   if (!matching) {
     return null;
