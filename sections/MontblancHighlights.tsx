23ffa7e5e8491e9aecbdd47e7297c16cbb4f57cf
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-04-26 01:54:21 -0300

    feat: add montblanc highlights

---
+++ sections/MontblancHighlights.tsx
@@ -0,0 +1,8 @@
+import Highlights from "deco-sites/bergerson/components/montblanc/Highlights.tsx";
+import type { Props } from "deco-sites/bergerson/components/montblanc/Highlights.tsx";
+
+function MontblancHighlights(props: Props) {
+  return <Highlights {...props} />;
+}
+
+export default MontblancHighlights;
