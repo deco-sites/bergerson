f99a7539e3cf7e45d9ca3f2ec14bff3238e6582d
Lucas Oliveira <l.alves.oliveira@hotmail.com>
2023-05-03 16:03:05 -0300

    fix: minor fixes

--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -16,6 +16,9 @@ function NavItem({ item }: { item: INavItem }) {
   const imageIndex = useSignal(0);
   const target = newPage ? "_blank" : "_self";
 
+  const baseChildrenClasses =
+    `overflow-hidden top-0 left-0 fixed max-h-0 z-50 w-screen mt-[${headerHeightDesktop}] opacity-0 bg-[#ffffffe0]`;
+
   return (
     <div class="group flex items-center">
       <a href={href} target={target} class="h-full px-6 flex items-center">
@@ -30,37 +33,50 @@ function NavItem({ item }: { item: INavItem }) {
       {children && children.length > 0 &&
         (
           <div
-            class={`px-[30px] py-[45px] fixed invisible hover:visible group-hover:visible bg-[#ffffffe0] z-50 flex items-start justify-start gap-6 w-screen mt-[${headerHeightDesktop}]`}
-            style={{ top: "0px", left: "0px" }}
+            class={`${baseChildrenClasses} hover:(max-h-full opacity-100 duration-700) group-hover:(max-h-full opacity-100 duration-700) transition-[max-height, opacity] duration-200 ease-in-out`}
           >
-            <img
-              alt={label}
-              loading="lazy"
-              decoding="async"
-              class="w-[300px] h-[300px]"
-              src={children[imageIndex.value].image?.src}
-            />
-            <ul class="flex flex-col flex-wrap items-start justify-center pl-20  max-h-[356px]">
-              {children.map((item, index) => (
-                <>
-                  <li
-                    value={index}
-                    class="group py-2.5 pr-20"
-                    onMouseLeave={() => (imageIndex.value = 0)}
-                    onMouseEnter={() => (imageIndex.value = index)}
-                  >
-                    <a href={item.href}>
-                      <Text
-                        class="uppercase hover:font-extrabold"
-                        variant="menu"
-                      >
-                        {item.label}
-                      </Text>
-                    </a>
-                  </li>
-                </>
-              ))}
-            </ul>
+            <div class="px-[30px] py-[45px] flex items-start justify-start gap-6 w-full">
+              <div class="w-[300px] h-[300px] relative">
+                {children.map((item, index) => {
+                  const isActive = index === imageIndex.value;
+                  const opacityClass = isActive
+                    ? "opacity-100 z-10"
+                    : "opacity-0";
+
+                  return (
+                    <img
+                      alt={label}
+                      loading="lazy"
+                      decoding="async"
+                      src={item.image?.src}
+                      class={`w-[300px] h-[300px] object-cover transition ease-in-out duration-1000 absolute ${opacityClass}`}
+                    />
+                  );
+                })}
+              </div>
+
+              <ul class="flex flex-col flex-wrap items-start justify-center pl-20  max-h-[356px]">
+                {children.map((item, index) => (
+                  <>
+                    <li
+                      value={index}
+                      class="group py-2.5 pr-20"
+                      onMouseLeave={() => (imageIndex.value = 0)}
+                      onMouseEnter={() => (imageIndex.value = index)}
+                    >
+                      <a href={item.href}>
+                        <Text
+                          class="uppercase hover:font-extrabold"
+                          variant="menu"
+                        >
+                          {item.label}
+                        </Text>
+                      </a>
+                    </li>
+                  </>
+                ))}
+              </ul>
+            </div>
           </div>
         )}
     </div>
