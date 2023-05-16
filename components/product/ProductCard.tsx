490b45e5fa78b08c20e35ebcc56e81fa1c72a7cd
Gustavo Vasconcellos <pvasconcellos.gustavo@gmail.com>
2023-05-16 02:40:46 -0300

    feat: improve product card features

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -18,6 +18,7 @@ function ProductCard({ product, preload, largeImage }: Props) {
     name,
     image: images,
     offers,
+    sku,
   } = product;
 
   const [front, back] = images ?? [];
@@ -30,8 +31,10 @@ function ProductCard({ product, preload, largeImage }: Props) {
       class="w-full group h-full"
       id={`product-card-${productID}`}
     >
-      <a href={url} aria-label="product link" class="h-full flex flex-col">
-        <div
+      <div class="h-full flex flex-col">
+        <a
+          href={url}
+          aria-label="product link"
           class={`relative w-full bg-white ${imageWidth} self-center grid grid-cols-1`}
         >
           <Image
@@ -52,14 +55,22 @@ function ProductCard({ product, preload, largeImage }: Props) {
             alt={back?.alternateName ?? front.alternateName}
             class="col-start-1 row-start-1 w-full opacity-0 group-hover:opacity-100 object-contain object-center transition ease-in-out duration-500"
           />
-        </div>
+        </a>
 
         <div class="flex flex-col gap-1 py-2 h-full">
-          <span class="text-[15px] uppercase font-heading-1 text-center mb-auto">
+          <a
+            href={url}
+            aria-label="product link"
+            class="text-[15px] uppercase font-heading-1 text-center mb-auto"
+          >
             {name}
-          </span>
+          </a>
 
-          <div class="py-2 flex items-center justify-between">
+          <a
+            href={url}
+            aria-label="product link"
+            class="py-2 flex items-center justify-between"
+          >
             {price && (
               <span class="font-semibold font-serif text-[#585858]">
                 10x {formatPrice(price / 10, offers!.priceCurrency!)}
@@ -68,13 +79,16 @@ function ProductCard({ product, preload, largeImage }: Props) {
             <span class="text-[15px]">
               {formatPrice(price, offers!.priceCurrency!)}
             </span>
-          </div>
+          </a>
 
-          <button class="uppercase py-2.5 border-t-1 border-b-1 border-[#ffd049] mt-2 text-lg font-bold">
+          <a
+            href={`https://www.bergersonjoias.com/checkout/cart/add?sku=${sku}&qty=1&seller=1&sc=1`}
+            class="uppercase py-2.5 border-t-1 border-b-1 border-[#ffd049] mt-2 text-lg font-bold text-center"
+          >
             Comprar
-          </button>
+          </a>
         </div>
-      </a>
+      </div>
     </div>
   );
 }
